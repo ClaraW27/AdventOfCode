c404771401c43dc6cc20b0a240becf37286e950c
GLOBAL\Ops.Clara.Wang <Clara.Wang@arup.com>
2023-12-06 14:19:32 +0000

    Day 6

---
+++ AoC/DaySixPartOne.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    internal class DaySixPartOne
+    {
+        Dictionary<int, int> document = new Dictionary<int, int>
+        {
+            {38,234 },
+            {67,1027 },
+            {76,1157 },
+            {73,1236 }
+        }; //{ time: distance }
+
+        public int GetWinWays(int maxSecond, int record)
+        {
+            int winWays = 0;
+
+            for (int i = 0; i < maxSecond; i++)
+            {
+
+                if (BeatRecord(i, maxSecond, record))
+                {
+                    winWays++;
+                }
+            }
+
+            return winWays;
+
+        }
+
+        public bool BeatRecord(int holdTime, int maxSecond, int record)
+        {
+            bool result = false;
+
+            int restTime = maxSecond - holdTime;
+            int speed = holdTime * 1;
+
+            int myDistance = speed * restTime;
+
+            result = Compare(myDistance, record);
+
+            return result;
+        }
+
+        public bool Compare(int myDistance, int recordDistance)
+        {
+            return (myDistance > recordDistance);
+        }
+
+        public void MySolution()
+        {
+            long multiplyNumbers = 1;
+            foreach (var item in this.document)
+            {
+                int myTime = item.Key;
+                int record = item.Value;
+                int winWays = GetWinWays(myTime, record);
+
+                if (winWays > 0)
+                {
+                    //Console.WriteLine(winWays);
+                    multiplyNumbers *= winWays;
+
+                }
+            }
+            Console.WriteLine("After we multiply the numbers:" + multiplyNumbers);
+        }
+    }
+}
---
+++ AoC/DaySixPartTwo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC
+{
+    internal class DaySixPartTwo
+    {
+        long time = 38677673;
+        long distance = 234102711571236;
+
+
+        public long GetWinWays(long maxSecond, long record)
+        {
+            long winWays = 0;
+
+            for (long i = 0; i < maxSecond; i++)
+            {
+
+                if (BeatRecord(i, maxSecond, record))
+                {
+                    winWays++;
+                }
+            }
+
+            return winWays;
+
+        }
+
+        public bool BeatRecord(long holdTime, long maxSecond, long record)
+        {
+            bool result = false;
+
+            long restTime = maxSecond - holdTime;
+            long speed = holdTime * 1;
+
+            long myDistance = speed * restTime;
+
+            result = Compare(myDistance, record);
+
+            return result;
+        }
+
+        public bool Compare(long myDistance, long recordDistance)
+        {
+            return (myDistance > recordDistance);
+        }
+
+        public void MySolution()
+        {
+            long myWinWays = GetWinWays(this.time, this.distance);
+            Console.WriteLine(" In this longer race my ways: " + myWinWays);
+        }
+    }
+}
